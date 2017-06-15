76816dae782dc86faec765106b32e3b885cbeb8b
chrisw90uk <chrisw90_uk@hotmail.com>
2017-06-15 22:47:57 +0100

    Skeleton of media library /w tab function

--- js/repriseApp.js
+++ js/repriseApp.js
@@ -4,6 +4,46 @@ app.controller("bannerImg",function($scope, $http){
 		$scope.imgs = response.data;
 	});
 });
+
+app.controller("mediaLibrary", function($scope, $http){
+	
+	//Tab function
+
+	$scope.activeTab = 0;
+
+	$scope.media = [
+		{
+			name: "Images",
+			class: "images"
+		},
+		{
+			name: "Audio",
+			class: "audio"
+		},
+		{
+			name: "Videos",
+			class: "videos"
+		}
+	];
+
+	$scope.changeTab = function(index){
+		$scope.activeTab = index;
+	};
+
+	$http.get("ctrl/images.php").then(function(response){
+		$scope.images = response.data;
+	});
+
+	$http.get("ctrl/audio.php").then(function(response){
+		$scope.audio = response.data;
+	});
+
+	$http.get("ctrl/video.php").then(function(response){
+		$scope.video = response.data;
+	});
+
+});
+
 /*
 
 app.controller("chatCtrl",function($scope, $http){
