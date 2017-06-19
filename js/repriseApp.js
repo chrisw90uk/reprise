86edbcd847bd25c848e94ba93a54b460ccdbede0
Chris Wickham <chrisw90_uk@hotmail.com>
2017-06-19 14:12:25 +0100

    edit date function

--- js/repriseApp.js
+++ js/repriseApp.js
@@ -53,6 +53,7 @@ app.controller("addDate",function($scope, $http){
 		$scope.currentDates = response.data;
 		console.log("retrieved");
 	})
+	
 	$scope.addDate = function(){
 		console.log("submitted");
 		var formData = JSON.stringify($scope.date);
@@ -68,13 +69,30 @@ app.controller("addDate",function($scope, $http){
 				console.log("retrieved");
 			});
 		})
-	}
-	
+	}	
 	
 	$scope.editDate = function(item){
 		$scope.editing = true;
 		$scope.editingDate = item;
 	}
+	
+	$scope.saveDate = function(id){
+		var formData = JSON.stringify($scope.editingDate);
+		$http({
+			method: 'POST',
+			url: 'ctrl/edit-date.php?id=' + id,
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+			data: formData
+		}).then(function(response){
+			alert(response.data);
+			$http.get("ctrl/current-dates.php").then(function(response){
+				$scope.currentDates = response.data;
+				console.log("retrieved");
+				$scope.editing = false;
+			});
+		})
+	}
+	
 	$scope.deleteDate = function(id){
 		$http.post('ctrl/delete-date.php?id=' + id).then(function(response){
 			alert(response.data);
@@ -84,6 +102,7 @@ app.controller("addDate",function($scope, $http){
 			})
 		});
 	}
+	
 	$scope.closePopup = function(){
 		$scope.editing = false;
 	}
