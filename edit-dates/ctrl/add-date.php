2dea56f67e3a3ae92d311e37b1238e9e44c7c848
chrisw90uk <chrisw90_uk@hotmail.com>
2017-07-16 13:19:53 +0100

    BUG FIXING FOR ADDING DATES: Added angular-ui as dependency, added wickedpicker for timepicker

--- edit-dates/ctrl/add-date.php
+++ edit-dates/ctrl/add-date.php
@@ -1,18 +1,26 @@
 <?php require 'config.php'; ?>
 <?php
 
+	date_default_timezone_set('Europe/London');
+
 	$instance = json_decode(file_get_contents("php://input"));
 	$id = bin2hex(openssl_random_pseudo_bytes(5));
-	$date = $instance->datetime;
+
+	$date = $instance->date;
+	$date = strtotime($date);
+	$date = date("Y-m-d", $date);
+
+	$time = $instance->time;
 	$event = $instance->name;
 	$venue = $instance->venue;
 	$location = $instance->location;
 	$tickets = $instance->tickets;
 	
-	$sql = "INSERT INTO reprisecwickham.dates (id, date, event, venue, location, tickets) VALUES (:id, :date, :event, :venue, :location, :tickets)";
+	$sql = "INSERT INTO reprisecwickham.dates (id, date, time, event, venue, location, tickets) VALUES (:id, :date, :time, :event, :venue, :location, :tickets)";
 	$newDate = $conn->prepare($sql);
 	$newDate->bindParam(":id", $id, PDO::PARAM_STR);
 	$newDate->bindParam(":date", $date, PDO::PARAM_STR);
+	$newDate->bindParam(":time", $time, PDO::PARAM_STR);
 	$newDate->bindParam(":event", $event, PDO::PARAM_STR);
 	$newDate->bindParam(":venue", $venue, PDO::PARAM_STR);
 	$newDate->bindParam(":location", $location, PDO::PARAM_STR);
--- edit-dates/ctrl/edit-date.php
+++ edit-dates/ctrl/edit-date.php
@@ -3,16 +3,18 @@
 
 	$instance = json_decode(file_get_contents("php://input"));
 	$id = htmlspecialchars($_GET['id']);
-	$date = $instance->datetime;
+	$date = $instance->date;
+	$time = $instance->time;
 	$event = $instance->event;
 	$venue = $instance->venue;
 	$location = $instance->location;
 	$tickets = $instance->tickets;
 	
-	$sql = "UPDATE reprisecwickham.dates SET date = :date, event = :event, venue = :venue, location = :location, tickets = :tickets WHERE id = :id";
+	$sql = "UPDATE reprisecwickham.dates SET date = :date, time = :time, event = :event, venue = :venue, location = :location, tickets = :tickets WHERE id = :id";
 	$editDate = $conn->prepare($sql);
 	$editDate->bindParam(":id", $id, PDO::PARAM_STR);
 	$editDate->bindParam(":date", $date, PDO::PARAM_STR);
+	$editDate->bindParam(":time", $time, PDO::PARAM_STR);
 	$editDate->bindParam(":event", $event, PDO::PARAM_STR);
 	$editDate->bindParam(":venue", $venue, PDO::PARAM_STR);
 	$editDate->bindParam(":location", $location, PDO::PARAM_STR);
